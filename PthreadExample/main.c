c2e7df8dd4ab59120a266e2a4d8e3b3d11c6fa0d
chlee-laptop <chlee4831@gmail.com>
2017-04-18 13:54:58 +0900

    mutex added

--- PthreadExample/main.c
+++ PthreadExample/main.c
@@ -2,13 +2,17 @@
 #include <pthread.h>
 
 int global = 1; //thread에서 접근할 전역변수
+pthread_mutex_t mutex;
+
 void *func(void *arg)
 {
 	int i;
 	for(i = 0; i < 10; i++)
 	{
+		pthread_mutex_lock(&mutex);
 		printf("therad%d: global = %d\n", (int)arg, global);
 		global = global*2; //전역변수에 2를 10번 곱하는 함수입니다.
+		pthread_mutex_unlock(&mutex);
 	}
 	pthread_exit(0);	//작업을 마치면 thread를 종료합니다.
 }
@@ -17,6 +21,7 @@ int main()
 {
 	int i;
 	pthread_t id[3];
+	pthread_mutex_init(&mutex, NULL);
 
 	for(i = 0; i < 3; i++)
 	{
@@ -38,7 +43,7 @@ int main()
 		}
 		printf("------------------Thread%d joined------------------\n",i);
 	}	//작업이 끝난 스레드를 join하고 실패할 경우 에러 출력
-
+	pthread_mutex_destroy(&mutex);
 	printf("End Main\n");	//모든 스레드가 join되면 main프로그램을 종료합니다.
 	return 0;
 }
